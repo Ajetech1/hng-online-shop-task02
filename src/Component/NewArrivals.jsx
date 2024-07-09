d7b1de03e3293b969c28e62ac867a800cb427658
Ajetech1 <jeleelajetunmobi@gmail.com>
2024-07-09 10:36:48 +0100

    new arrivals mobile responsible and cuttoff price added

--- src/Component/NewArrivals.jsx
+++ src/Component/NewArrivals.jsx
@@ -12,70 +12,68 @@ import { Button, Container, Row, Col } from "react-bootstrap";
 function NewArrivals() {
   return (
     <div className="new-arrivals">
-      <h1 className="mt-4 text-center ">New Arrivals</h1>
-      <div className="d-flex justify-content-center gap-3 mt-4">
-        <ProductCard
-          image={Image1}
-          title="T-shirt with Tape Details"
-          rating={4.5}
-          price={5000}
-        />
-        <ProductCard
-          image={Image2}
-          title="Skinny Fit Jeans"
-          rating={3.5}
-          price={5000}
-        />
-        <ProductCard
-          image={Image3}
-          title="Checkered Shirt"
-          rating={4.5}
-          price={7000}
-        />
-        <ProductCard
-          image={Image4}
-          title="Sleeve Striped T-Shirt"
-          rating={4.5}
-          price={2000}
-        />
-      </div>
-      {/* <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "40vh" }}
-      >
-        <ReusableButton
-          variant="light"
-          size="md"
-          className="mt-5 rounded-5 px-5 custom-button"
-          onClick={() => console.log("You are Now Veiwing All Our Products!")}
-        >
-          View All
-        </ReusableButton>
-      </div> */}
-
-      <Container
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "40vh" }}
-      >
-        <Row className="w-100">
-          <Col
-            xs={12}
-            md={3}
-            lg={2}
-            className="d-flex justify-content-center mx-auto"
-          >
-            <ReusableButton
-              variant="light"
-              size="md"
-              className="mt-5 rounded-5 px-5 custom-button w-100"
-              onClick={() =>
-                console.log("You are Now Viewing All Our Products!")
-              }
-            >
-              View All
-            </ReusableButton>
+      <h1 className="mt-4 text-center">New Arrivals</h1>
+      <Container>
+        <Row className="mt-4 ">
+          <Col xs={6} sm={6} md={3}>
+            <ProductCard
+              image={Image1}
+              title="T-shirt with Tape Details"
+              rating={4.5}
+              price={5000}
+            />
+          </Col>
+          <Col xs={5} sm={6} md={3}>
+            <ProductCard
+              image={Image2}
+              title="Skinny Fit Jeans"
+              rating={3.5}
+              price={5000}
+              cutoffPrice="₦10,000"
+            />
+          </Col>
+          <Col className="column-adjust" xs={6} sm={6} md={3}>
+            <ProductCard
+              image={Image3}
+              title="Checkered Shirt"
+              rating={4.5}
+              price={7000}
+            />
+          </Col>
+          <Col className="column-adjust" xs={6} sm={6} md={3}>
+            <ProductCard
+              image={Image4}
+              title="Sleeve Striped T-Shirt"
+              rating={4.5}
+              price={2000}
+              cutoffPrice="₦5,500"
+            />
           </Col>
         </Row>
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "40vh" }}
+        >
+          <Row className="w-100">
+            <Col
+              xs={12}
+              md={3}
+              lg={2}
+              className="d-flex justify-content-center mx-auto"
+            >
+              <ReusableButton
+                variant="light"
+                size="md"
+                className="mt-5 rounded-5 px-5 custom-button w-100"
+                onClick={() =>
+                  console.log("You are Now Viewing All Our Products!")
+                }
+              >
+                View All
+              </ReusableButton>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     </div>
   );
--- src/Component/ProductCard.jsx
+++ src/Component/ProductCard.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import StarRatings from "react-star-ratings";
+import "../assets/css/ProductCard.css";
 
-const ProductCard = ({ image, title, rating, price }) => {
+const ProductCard = ({ image, title, rating, price, cutoffPrice }) => {
   return (
-    <Card style={{ width: "18rem", height: "18rem", borderRadius: "16px" }}>
+    <Card className="custom-card">
       <Card.Img variant="top" src={image} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className="custom-title">{title}</Card.Title>
         <div style={{ display: "flex", alignItems: "center" }}>
           <StarRatings
             rating={rating}
             starRatedColor="gold"
             numberOfStars={5}
-            starDimension="20px"
+            starDimension="12px"
             starSpacing="2px"
           />
           <span style={{ marginLeft: "10px" }}>{rating}/5</span>
         </div>
         <Card.Text>
           <strong>₦{price}</strong>
+          <span style={{ textDecoration: "line-through", marginLeft: "10px" }}>
+            {cutoffPrice}
+          </span>
         </Card.Text>
       </Card.Body>
     </Card>
